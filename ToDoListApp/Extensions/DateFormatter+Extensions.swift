9774dd6aad2250ec39474a816714fbc364daa766
mashaler <mashalerolva@gmail.com>
2024-09-02 21:29:44 +0200

    added extension for the date formatter

--- ToDoListApp/Extensions/DateFormatter+Extensions.swift
+++ ToDoListApp/Extensions/DateFormatter+Extensions.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+extension Date {
+    func toString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: self)
+    }
+    
+    func toTimeString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        return formatter.string(from: self)
+    }
+}
+
+extension String {
+    func toDate() -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.date(from: self)
+    }
+    
+    func toTime() -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        return formatter.date(from: self)
+    }
+}
