b8386eb40dbe4a61c559647193e398b6ae1d373e
mashaler <mashalerolva@gmail.com>
2024-09-02 19:31:16 +0200

    added To do list app

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Rolva Mashale on 2024/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
